1bc26f5f3d3d2240e30d05b76c956ab763c1a3d7
luu2615 <luu26515@gmail.com>
2026-07-03 19:47:56 -0300

    Parte uno terminada, transaccionalidad implementada

--- src/main/java/ar/edu/unahur/obj2/energia/Rutina.java
+++ src/main/java/ar/edu/unahur/obj2/energia/Rutina.java
@@ -12,8 +12,29 @@ public class Rutina implements Operacion {
 
     @Override
     public void ejecutar() throws ReservaFisicaException {
+        List<Operacion> operacionesEjecutadas = new ArrayList<>();
+        try {
+        for (Operacion operacion : operaciones) {
+            operacion.ejecutar();
+            operacionesEjecutadas.add(operacion);
+        }
+
+        operaciones.clear();
+        
+        } catch (ReservaFisicaException e) {
+
+        for (int i = operacionesEjecutadas.size() - 1; i >= 0; i--) {
+            operacionesEjecutadas.get(i).deshacer();
+        }
+
+        throw e;
+        }
     }
 
     @Override
-    public void deshacer() throws ReservaFisicaException {}
+    public void deshacer() throws ReservaFisicaException {
+        for (int i = operaciones.size() - 1; i >= 0; i--) {
+        operaciones.get(i).deshacer();
+        }
+    }
 }
